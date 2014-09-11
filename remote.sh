6d30ea55ab8d6673981ad074dea5a04f8b1ec9f6
sinfallas <sinfallas@gmail.com>
2014-09-10 22:27:27 -0430

    * subida de archivos

---
+++ remote.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+HOST="aqui_pon_la_maquina_remota"
+USER="aqui_pon_el_usuario_remoto"
+PASS="aqui_pon_el_password_remoto"
+CMD=$@
+VAR=$(expect -c "
+spawn ssh -o StrictHostKeyChecking=no $USER@$HOST $CMD
+match_max 100000
+expect \"*?assword:*\"
+send -- \"$PASS\r\"
+send -- \"\r\"
+expect eof
+")
+echo "==============="
+echo "$VAR"
+exit 0
